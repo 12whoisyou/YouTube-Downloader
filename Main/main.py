5f615ae8f277a98051f91c13c2e5a297bb972a33
red cube <66546815+12whoisyou@users.noreply.github.com>
2022-02-11 16:23:58 +0100

    Update main.py

--- Main/main.py
+++ Main/main.py
@@ -8,38 +8,10 @@ from selenium import webdriver
 # I will polish the code tomrow
 
 file_path = []
+website = "http://10.0.0.5/"
+os.chdir(os.getcwd())
+
 def start():
-    print("hello, welcome to Music Downloader 1000")
-    prefred_type = input("Would you like to download music by link or name? ")
-
-    if prefred_type == "name":
-        by_name()
-    elif prefred_type == "link":
-        by_link()
-    else:
-        start()
-
-
-def by_link():
-    link = input("Enter link here: ")
-
-    url = YouTube(link)
-    print(url.title)
-    print("downloading....")
-
-    video = url.streams.filter(only_audio=True).first()
-    
-
-    #path_to_download_folder = str(os.path.join(Path.home(), "Downloads"))
-    #video.download(path_to_download_folder, filename=url.title+'.mp3')
-    video.download()
-    print(url.title+'.mp4')
-    print(os.getcwd())
-
-
-    print("Downloaded! :)")
-
-def by_name():
     names = input("Enter song name and creator here: ")
     names = names.split(",")
     for count, name in enumerate(names):
@@ -65,24 +37,26 @@ def download(name):
     video.download(filename=video.title+'.mp4')
     print(video.title+'.mp4')
 
-    os.chdir(os.getcwd())
     os.rename(video.title+'.mp4', video.title+'.mp3')
+
+
 def upload_to_phone():
     driver = webdriver.Chrome()
-    driver.get("http://10.0.0.5/")
+    driver.get(website)
     time.sleep(10)
 
+    input = driver.find_element_by_id("fileupload")
     for file in file_path:
-        input = driver.find_element_by_id("fileupload")
         input.send_keys(os.path.abspath(file))
+    
+    file_loader = driver.find_element_by_class_name("uploading")
+    
     driver.find_element_by_id("upload-file").click()
+    
+    while file_loader.is_displayed():
+        time.sleep(3)
 
-    time.sleep(len(file_path)*5)
     [os.remove(file) for file in file_path]
 
 if __name__ == '__main__':
-    #start()
-    #by_link()
-    #print(os.listdir())
-    #os.rename(url.title+'.mp4', url.title+'.mp3')
-    by_name()
+    start()
